79bed76cb13d0dca84ec2b4601f57b69819f87e0
Bondarenko-Maksym <155956848+Bondarenko-Maksym@users.noreply.github.com>
2024-01-15 12:10:13 +0200

    Update ConsoleApplication1.cpp

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,20 +1,65 @@
-﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+// Бондаренко Максим, варіант 1
 //
 
 #include <iostream>
-
+using namespace std;
+void Fill_arr(int* arr, int n) {
+    for (int i = 0; i < n; i++)
+        *(arr + i)=rand()%41-20;}
+void Print_arr(int* arr, int n) {
+    for (int i = 0; i < n; i++)
+        cout<<*(arr + i)<<" ";}
+void Del_3(int *&arr, int &n) {
+    int* newarr = new int[n--];
+    for (int i = 0; i < 2; i++)
+        newarr[i] = arr[i];
+    for (int i = 2; i < n; i++)
+        newarr[i] = arr[i+1];
+    delete[] arr;
+    arr = newarr;
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+    int x;
+start:
+    cout << "Choose task:";
+    cin >> x;
+    switch (x) {
+    case 1: {
+        int n=15;
+        int* arr = new int[n];
+        Fill_arr(arr, n);
+        Print_arr(arr, n);
+        cout << endl;
+        Del_3(arr, n);
+        Print_arr(arr, n);
+    }break;
+    case 2: {
+        int n = 15, N;
+        int* arr = new int[n];
+        Fill_arr(arr, n);
+        Print_arr(arr, n);
+        cout << endl;
+        cout << "Print your number N:";
+        cin >> N;
+        int* newarr = new int[n++];
+        for (int i = 0; i < 2; i++)
+            newarr[i] = arr[i];
+        newarr[2] = N;
+        for (int i = 3; i < n; i++)
+            newarr[i] = arr[i - 1];
+        delete[] arr;
+        arr = newarr;
+        Print_arr(arr, n);
+    }break;
+    case 3: {
+        
+    }break;
+    default: {
+        cout << "Виберіть задачу 1-3";
+        goto start;
+    }
+    }
+    return 0;
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
